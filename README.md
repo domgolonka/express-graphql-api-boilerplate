c1475cb0337bb7e54e44d2a5708d397b2ecb9e84
denneulin <a.denneulin@gmail.com>
2016-07-05 16:20:51 +0200

    gulp babel eslint

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # express-graphql-api-boilerplate
 A boilerplate to kickstart a GraphQL API base on apollostack
+
+Heavily inspired by https://github.com/KunalKapadia/express-mongoose-es6-rest-api/
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var app = express();
+import express from 'express';
+const app = express();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
+  // eslint-disable-next-line
   console.log('Example app listening on port 3000!');
 });
